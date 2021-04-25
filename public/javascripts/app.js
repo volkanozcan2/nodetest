c7ce87f21fb5edbd21d45cc1599c65fa1a393c38
volkan <volqii@gmail.com>
2021-04-26 01:28:29 +0300

    initial

---
+++ public/javascripts/app.js
@@ -0,0 +1,2 @@
+const ip = document.querySelector('meta[name="author"]').content
+alert(ip)
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+  res.render('index', { title: 'Express', test: ip });
+});
+
+module.exports = router;
